02e5b19016ece510be70dd1ed4573c5fe0941a81
i-am-vietnam <haule24112001@gmail.com>
2023-09-26 11:12:04 +0700

    da xong lop

---
+++ src/main/java/com/example/testintellijgithub/hea.java
@@ -0,0 +1,17 @@
+package com.example.testintellijgithub;
+
+public class hea {
+    private int toi;
+
+    public hea(int toi) {
+        this.toi = toi;
+    }
+
+    public int getToi() {
+        return toi;
+    }
+
+    public void setToi(int toi) {
+        this.toi = toi;
+    }
+}
